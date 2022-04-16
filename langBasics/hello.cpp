ada7538f70e923e96c5fc10d0fa6f2534ec12f8c
vaibhavkumar5 <mrkumarvaibhav2001@gmail.com>
2022-04-17 00:53:44 +0530

    basic

--- langBasics/hello.cpp
+++ langBasics/hello.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main  (){
-    cout << "hello" << endl;
+int main() {
+    
+    int n = 10;
+    for (int i =1; i <= n; i++) {
+        cout << i*2 << endl;
+    }
+    
+
+    /*
+    int num;
+    cout << "enter value of num"<< endl;
+    cin>> num;
+
+    if (num>0) {
+        cout << "number is positive" << endl;
+    }
+    else if (num<0){
+        cout << "num is negative" << endl;
+    }
+    else {
+        cout << "num is 0" << endl;
+    }
+    */
+
+    // switch case
+
+/*
+    switch (num) {
+        case 0: cout << "im in case 0" <<endl;
+        break;
+        case 7: cout << "in 7" << endl;
+        break;
+        default : cout << "default " << endl;
+    }
+*/
+
 }
