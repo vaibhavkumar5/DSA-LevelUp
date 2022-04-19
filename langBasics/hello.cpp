ac78b7401fea2050905a6512b549dcd073518d95
vaibhavkumar5 <mrkumarvaibhav2001@gmail.com>
2022-04-19 10:33:59 +0530

    patterns

--- langBasics/hello.cpp
+++ langBasics/hello.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
+
+    int n;
+    cin>>n;
+
+    int i = 2;
+    int sum = 0;
+    while(i<=n){
+        sum = i + sum;
+        i+=2;
+
+    }
+    cout << sum;
+    /*
+    int n;
+    cout << "enter n" << endl;
+    cin >> n;
+
+    for (int i = 1; n >= i; i++)
+    {
+        cout << i << " " ;
+    }
+
     
     int n = 10;
     for (int i =1; i <= n; i++) {
         cout << i*2 << endl;
     }
-    
 
-    /*
+
     int num;
     cout << "enter value of num"<< endl;
     cin>> num;
@@ -27,14 +49,13 @@ int main() {
 
     // switch case
 
-/*
-    switch (num) {
-        case 0: cout << "im in case 0" <<endl;
-        break;
-        case 7: cout << "in 7" << endl;
-        break;
-        default : cout << "default " << endl;
-    }
-*/
-
+    /*
+        switch (num) {
+            case 0: cout << "im in case 0" <<endl;
+            break;
+            case 7: cout << "in 7" << endl;
+            break;
+            default : cout << "default " << endl;
+        }
+    */
 }
