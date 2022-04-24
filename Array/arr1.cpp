f5a4c34d77dcb5334308bb95968f4eb5a4585fd0
vaibhavkumar5 <mrkumarvaibhav2001@gmail.com>
2022-04-24 21:25:40 +0530

    patterns

---
+++ Array/arr1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[5] = {1,2,3,4,5};
+    int arr1[] = {1,1,1,1,1,1};
+    int arr2[15] = {0};
+
+    cout<< arr[3]<<endl;
+
+ return 0;
+}
