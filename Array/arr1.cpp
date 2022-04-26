6c1e59ead7eb5c8a558bd39e2c13ebf5478ed16c
vaibhavkumar5 <mrkumarvaibhav2001@gmail.com>
2022-04-26 19:43:36 +0530

    array

--- Array/arr1.cpp
+++ Array/arr1.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 using namespace std;
 
+void printArray(int arr[], int size){
+    for (int i=0; i<size; i++){
+        cout<< arr[i]<< " ";
+    }
+}
+
 int main()
 {
+    int arr[5]= {1,2,3,4,5};
+    printArray(arr, 5);
 
-    int arr[5] = {1,2,3,4,5};
-    int arr1[] = {1,1,1,1,1,1};
-    int arr2[15] = {0};
-
-    cout<< arr[3]<<endl;
 
  return 0;
 }
--- patterns/pattern9.cpp
+++ patterns/pattern9.cpp
@@ -2,6 +2,5 @@
 using namespace std;
 int main()
 {
- int a=2;
  
 }
