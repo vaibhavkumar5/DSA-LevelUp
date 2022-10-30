b73af6f3dc81ca9e6fa50ed31be40b216235fb5e
vaibhavkumar5 <mrkumarvaibhav2001@gmail.com>
2022-10-30 22:33:13 +0530

    BinarySearchQs

---
+++ binarySearch/firstocurrance.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int firstOcc(int arr[], int size, int target) {
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end- start)/2;
+    int ans = -1;
+
+    while (start <= end){
+        int midElement = arr[mid];
+        if (midElement == target){
+            ans = mid;
+            end = mid -1 ;
+        }
+        else if (target < midElement){
+            end = mid - 1;
+        } else  {
+            start = mid +1;
+        }
+         
+
+        mid = start + (end- start)/2;
+    }
+    return ans;
+}
+int main() {
+    
+    int arr[] = {2,3,3,3,4,7,9};
+    int size = 7;
+    int target = 3;
+
+   cout << firstOcc(arr, size, target) <<endl;
+    
+
+    return 0;
+}     
---
+++ binarySearch/implementaion.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ /*
+int binarySearch(int arr[], int size, int target) {
+    int start = 0;
+    int end = size - 1;
+    int mid = start/2 + end/2;
+
+    while (start <= end){
+        int midElement = arr[mid];
+
+        if (midElement == target){
+            return mid;
+        }
+        if (target < midElement){
+            end = mid - 1;
+        } else {
+            start = mid +1;
+        }
+         
+
+        mid = start/2 + end/2;
+    }
+    return -1;
+}
+*/
+int main() {
+    /*
+    int arr[] = {2,4,6,8,9,11,23};
+    int size = 7;
+    int target = 2;
+
+   cout << binarySearch(arr, size, target) <<endl;
+    */
+
+   binary_search({2,4,6,8,9,11,23}, )
+    return 0;
+}     
