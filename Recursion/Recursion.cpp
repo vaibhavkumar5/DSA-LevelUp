c476a3232227cb5821df9385baf74fe54cf0fbaa
vaibhav kumar <77304656+vaibhavkumar5@users.noreply.github.com>
2023-10-01 00:26:54 +0530

    recursion

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -73,11 +73,23 @@ int fact(int n){
 } // do the same in parameter 
 
 // reverse an array using recursion
+void rev(int arr[], int i, int n){
+   if(i>=n){
+    return;
+    }else{
+        swap(arr[i],arr[n]);
+        rev(arr,i+1,n-1);
+    }
+}
 
 
 
 int main(){
-    int n=3;
-    cout<< fact(n);
+    int arr[]={1,2,3,4,5};
+    rev(arr,0,4);
+    for(int i=0;i<5;i++){
+        cout<< arr[i];
+    }
+    
     return 0;
 }
